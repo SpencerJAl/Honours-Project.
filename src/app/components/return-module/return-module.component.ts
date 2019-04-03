ed69c03f048e7b51f0f0dd5a1966e426015dba1b
Spencer <40323886@live.napier.ac.uk>
2019-04-03 14:58:23 +0100

    finished view page, nearly finished return page

--- src/app/components/return-module/return-module.component.ts
+++ src/app/components/return-module/return-module.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Loan } from '../../models/loans';
-//import { CheckboxModule } from 'primgeng/checkbox';
+
+
 @Component({
   selector: 'app-return-module',
   templateUrl: './return-module.component.html',
@@ -9,6 +10,7 @@ import { Loan } from '../../models/loans';
 export class ReturnModuleComponent implements OnInit {
 
   loans:Loan[];
+
 
   constructor() { }
 
