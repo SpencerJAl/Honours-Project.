4c026c3c4009af0771ce2596f02a31a224f95848
Spencer <40323886@live.napier.ac.uk>
2019-04-03 16:29:25 +0100

    started setting up example login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//import { ToastModule } from 'primeng/toast';
+//import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
    title ="login";
    
-  constructor() { }
+  constructor(//private messageService: MessageService//
+    ) { }
+ //if(userID =="00000001" && password=="password"){
+ // addSingle() {
+  //  this.messageService.add({severity:'success', summary:'Service Message', detail:'Via MessageService'});
+//}
+
+//  addBad(){
+ //   this.messageService.add({severity:'warn', summary:'Login failed please try again', detail:'Via MessageService'});
+ // }
+
+ //}
 
   ngOnInit() {
   }
